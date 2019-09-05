ccbcdf68c5760fbf2e9932b70e610d698450541c
SamuelCampo <samuelcampo18@gmail.com>
2019-09-05 13:07:27 -0500

    Update Formato.php

--- api/application/models/Formato.php
+++ api/application/models/Formato.php
@@ -9,12 +9,12 @@ public function RegistrarFormato($datformato,$idformato_t2 = "")
 			$datformato = (object)$this->input->post();
 		}
 
-	$arr_formato['titulo_t2'] = $datformato->titulo_t2;
-	$arr_formato['descripcion_t2'] = $datformato->descripcion_t2;
-	$arr_formato['fmod_t2'] = date('Y-m-d');
-	$arr_formato['usumod_t2'] = $datformato->usumod_t2;
+			$arr_formato['titulo_t2'] = $datformato->titulo_t2;
+			$arr_formato['descripcion_t2'] = $datformato->descripcion_t2;
+			$arr_formato['fmod_t2'] = date('Y-m-d');
+			$arr_formato['usumod_t2'] = $datformato->usumod_t2;
 
-	$this->db->where('idformato_t2', $datformato->idformato_t2);
+			$this->db->where('idformato_t2', $datformato->idformato_t2);
 			$query = $this->db->get('ps_formato_t2');
 			if ($query->row() > 0) {
 				$this->db->where('idformato_t2', $datformato->idformato_t2);
