2830967ab434d7b3736e37731116652692c059c7
SamuelCampo <samuelcampo18@gmail.com>
2019-09-05 11:18:13 -0500

    Error de carga de modelo sesiones mal escrito

--- api/application/controllers/Formatos.php
+++ api/application/controllers/Formatos.php
@@ -1,8 +1,4 @@
 <?php
-if( ! ini_get('date.timezone') )
-{
-    date_default_timezone_set('GMT');
-}
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Formatos extends CI_Controller {
@@ -10,10 +6,12 @@ class Formatos extends CI_Controller {
 public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('Tercero');
-		$this->load->model('Usuario');
-		$this->load->model('Formato');
-		$this->load->model('Session');
+		$this->load->model('formato');
+	}
+
+	public function index()
+	{
+		echo "hola";
 	}
 
 	public function Formato()
@@ -28,7 +26,7 @@ public function __construct()
 			case 'gestionar':
 				if ($this->uri->segment(4) == "guardar") {
 				echo json_encode($this->input->post());
-				$query = $this->Formato->RegistrarFormato();
+				$query = $this->formato->RegistrarFormato();
 				echo json_encode($query);
 				}
 				break;
