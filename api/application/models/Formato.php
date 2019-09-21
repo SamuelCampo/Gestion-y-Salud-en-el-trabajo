67310d082c64b1c51f5b1d2bbca935df0e0ddf0c
Miguelort095 <31443985+Miguelort095@users.noreply.github.com>
2019-09-21 18:00:04 -0500

    act

--- api/application/models/Formato.php
+++ api/application/models/Formato.php
@@ -13,14 +13,33 @@ public function RegistrarFormato($datformato = "" ,$idformato_t2 = "")
 			if(!empty($datformato->categoria_t2))$arr_formato['categoria_t2'] =$datformato->categoria_t2;
 			$arr_formato['fmod_t2'] = date('Y-m-d h:i:s');
 			$arr_formato['usumod_t2'] = $datformato->usumod_t2;
+			var_dump($datformato);
 			$this->db->where('idformato_t2', intval($datformato->idformato_t2));
 			$query = $this->db->get('ps_formato_t2');
 			if (count($query->result()) > 0) {
 				$this->db->where('idformato_t2', intval($datformato->idformato_t2));
-				return $this->db->update('ps_formato_t2', $arr_formato);
+				 $this->db->update('ps_formato_t2', $arr_formato);
+				$id = $datformato->idformato_t2;
 			}else {
 				return $this->db->insert('ps_formato_t2', $arr_formato);
+				$id = $this->db->insert_id();
 			}
+			  $categoria = explode(',', $datformato->categorias);
+			  for ($i = 0; $i < count($categoria); $i++) {
+			 	
+			 	
+			 $this->db->where('idcategoria_t8',$categoria[$i]);
+			 $query = $this->db->get('ps_categoria_t8')->row();
+
+			  $arr_relacion['idformato_t6'] = $id;
+			  $arr_relacion['idcategoria_t6'] = $categoria[$i];
+			  $arr_relacion['tituloformato_t6'] = $datformato->titulo_t2;
+			  $arr_relacion['categoria_t6'] = $query->nombre_t8;
+			  $arr_relacion['fmod_t6'] = date('Y-m-d h:i:s');
+			  $arr_relacion['umod_t6'] = $datformato->usumod_t2;
+			  return $this->db->insert('ps_categoria_for_t6', $arr_relacion);
+			  }
+
 }	
 
 	public function ConsultaFormato($idformato_t2 = "")
